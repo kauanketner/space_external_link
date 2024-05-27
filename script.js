392d3e922df87b2f7101d00422cee595ee7d9050
Kauan <kauanketner@icloud.com>
2024-05-26 22:55:10 -0300

    refatoração

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.getElementById("theme-toggle")
-  const icon = document.getElementById("toggle-icon")
 
   toggle.addEventListener("click", () => {
     toggle.classList.toggle("active")
